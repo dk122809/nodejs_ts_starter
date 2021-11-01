4c3c45a0b863f89366848bc959644432f74c91b6
deepak <deepak.sveltetech@gmail.com>
2021-11-02 00:42:27 +0530

    checking husky.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import { config } from './config/vars';
+import { config } from "./config/vars";
 import app from "./config/server";
-const test = 0 ;
 
 app.listen(config.port, () => {
-    console.log(`Listening on port: ${config.port}`)
-})
+  console.log(`Listening on port: ${config.port}`);
+});
